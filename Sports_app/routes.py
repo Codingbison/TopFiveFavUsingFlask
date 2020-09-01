65a6d86a4066051c63a95c4307def24b0ab14d7f
Nibras Muhsin <65922187+Codingbison@users.noreply.github.com>
2020-09-01 15:19:20 -0700

    Update routes.py

--- Sports_app/routes.py
+++ Sports_app/routes.py
@@ -4,6 +4,11 @@ from flask import render_template
 # home route
 @app.route('/')
 def home():
+    return render_template("home.html")
+
+@app.route('/favouriate')
+def favouriate():
     item_dict = {1: "Sami Hyypia", 2: "Harry Kewell", 3:"Stephane Henchoz", 4:"Jerzy Dudek", 5:"Markus Babbel"}
-    return render_template("home.html", item_dict=item_dict)
+    return render_template("register.html", item_dict=item_dict)
+    
 
